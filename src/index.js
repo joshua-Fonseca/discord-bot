a522bd91570fdc80cb0161d050916f98bbf3208b
Joshua Fonseca <jfonse01@uoguelph.ca>
2025-03-19 21:52:55 -0400

    make bot respond to hello and otherwise log the message content. hid token in a .env variable for the bot

---
+++ src/index.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+
+// https://discordjs.guide/popular-topics/intents.html#privileged-intents
+// https://discord.com/developers/docs/events/gateway
+
+const { Client, IntentsBitField } = require("discord.js");
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent
+    ]
+});
+
+client.on("ready", (c) => {
+    // console.log("ready");
+    console.log(c.user.tag, "is online");
+});
+
+// https://discord.js.org/docs/packages/discord.js/14.18.0/Message:Class
+client.on("messageCreate", (message) => {
+    // console.log(message.content);
+
+    if (message.author.bot) {
+        return;
+    }
+
+    if (message.content === "hello") {
+        message.reply("hello");
+    }
+    else {
+        console.log(message.author.username, "said", message.content);
+    }
+})
+
+client.login(process.env.TOKEN);
